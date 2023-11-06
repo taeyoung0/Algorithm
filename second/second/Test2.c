389b9c1b57666b7c94c46a42c2328eed0c074293
TaeYoungJeon <https://github.com/taeyoung0/animated-octo-train.git>
2023-11-06 16:08:02 +0900

    study

--- second/second/Test2.c
+++ second/second/Test2.c
@@ -1,52 +1,52 @@
-#include <stdio.h>
-
-void main()
-{
-	//int A = 1;
-	//int sum = 0;
-	//int B = 0;
-	//int C = 0;
-
-	//do
-	//{
-	//	A = 1;
-	//	B = 0;
-	//	C = C + 1;
-	//	do {
-	//		B = B + 1;
-	//		A = A * B;
-	//	} while (B != C);
-	//	sum = sum + A;
-	//} while (C < 10);
-	//printf("%d", sum);
-
-	int A;
-	int B;
-	int LCM;
-	int GCD;
-	int C;
-	int S;
-	int remainder;
-
-	scanf_s("%d", &A);
-	scanf_s("%d", &B);
-	
-	C = A * B;
-
-	if (A < B)
-	{
-		S = A;
-		A = B;
-		B = S;
-	}
-	do
-	{
-		remainder = A % B;
-		A = B;
-		B = remainder;
-	} while (remainder != 0);
-	GCD = A;
-	LCM = C / GCD;
-	printf("최대공약수 : %d", GCD);
-	printf("\n최소공배수 : %d", LCM);
-}
+//#include <stdio.h>
+//
+//void main()
+//{
+//	//int A = 1;
+//	//int sum = 0;
+//	//int B = 0;
+//	//int C = 0;
+//
+//	//do
+//	//{
+//	//	A = 1;
+//	//	B = 0;
+//	//	C = C + 1;
+//	//	do {
+//	//		B = B + 1;
+//	//		A = A * B;
+//	//	} while (B != C);
+//	//	sum = sum + A;
+//	//} while (C < 10);
+//	//printf("%d", sum);
+//
+//	int A;
+//	int B;
+//	int LCM;
+//	int GCD;
+//	int C;
+//	int S;
+//	int remainder;
+//
+//	scanf_s("%d", &A);
+//	scanf_s("%d", &B);
+//	
+//	C = A * B;
+//
+//	if (A < B)
+//	{
+//		S = A;
+//		A = B;
+//		B = S;
+//	}
+//	do
+//	{
+//		remainder = A % B;
+//		A = B;
+//		B = remainder;
+//	} while (remainder != 0);
+//	GCD = A;
+//	LCM = C / GCD;
+//	printf("최대공약수 : %d", GCD);
+//	printf("\n최소공배수 : %d", LCM);
+//}
---
+++ second/second/arr.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+
+//void main() {
+	//int A[10], MAX, i = 0;
+
+	//do {
+	//	scanf_s("%d", &A[i]);
+	//	i = i + 1;
+	//} while (i < 10);
+
+	//MAX = A[0];
+	//i = 1;
+
+	//do
+	//{
+	//	if (MAX < A[i])
+	//	{
+	//		MAX = A[i];
+	//	}
+	//	i = i + 1;
+	//} while (i < 10);
+	//printf("최대값은 : %d" ,MAX);
+	// --------------------------------------
+
+	//int A[10], MAX, i = 0;
+	//int MIN = 0;
+
+	//for (i = 0; i <= 9;i++)
+	//	scanf_s("%d",&A[i]);
+
+	//MAX = A[0];
+	//MIN = A[0];
+
+	//for (i = 0; i <= 9; i++)
+	//{
+	//	if (MAX < A[i])
+	//	{
+	//		MAX = A[i];
+	//	}
+	//	else if(MIN > A[i])
+	//	{
+	//		MIN = A[i];
+	//	}
+	//}
+	//printf("최대값은 : %d", MAX);
+	//printf("최소값은 : %d", MIN);
+	// -------------------------------------
+
+	//int A[10], MAX, i = 0;
+	//int MIN = 0;
+	//int sum = 0;
+	//float avg;
+
+	//for (i = 0; i <= 9;i++)
+	//	scanf_s("%d",&A[i]);
+
+	//MAX = A[0];
+	//MIN = A[0];
+
+	//for (i = 0; i <= 9; i++)
+	//{
+	//	sum = sum + A[i];
+	//	if (MAX < A[i])
+	//	{
+	//		MAX = A[i];
+	//	}
+	//	else if(MIN > A[i])
+	//	{
+	//		MIN = A[i];
+	//	}
+	//}
+	//avg = sum - MAX - MIN;
+	//printf("최대값은 : %d", MAX);
+	//printf("최소값은 : %d", MIN);
+	//printf("평균점수는 %f", avg / 8);
+//}
