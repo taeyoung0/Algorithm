ddf528d1b9dd8de7b1cb4d4b2327c82eead4d0dd
TaeYoungJeon <https://github.com/taeyoung0/animated-octo-train.git>
2023-11-10 10:14:22 +0900

    Sort

--- second/second/Test2.c
+++ second/second/Test2.c
@@ -1,7 +1,7 @@
-//#include <stdio.h>
-//
-//void main()
-//{
+#include <stdio.h>
+#include <limits.h>
+void main()
+{
 //	//int A = 1;
 //	//int sum = 0;
 //	//int B = 0;
@@ -49,4 +49,105 @@
 //	LCM = C / GCD;
 //	printf("최대공약수 : %d", GCD);
 //	printf("\n최소공배수 : %d", LCM);
-//}
+// -----------------------------------
+
+	/*int i = 0;
+	int sum = 0;
+	int cnt = 0;
+
+	do
+	{
+		i = i + 1;
+		if (i % 5 == 0)
+		{
+			cnt = cnt + 1;
+			sum = sum + i;
+		}
+	}while (i < 100);
+
+	printf("%d", sum);
+	printf("\n%d", cnt);
+	*/
+
+	// 위에 문제를 for문으로 바꿔봄
+
+	/*int i = 0;
+	int sum = 0;
+	int cnt = 0;
+	
+	for (i = 0; i <= 100; i++)
+	{
+		if (i % 5 == 0)
+		{
+			sum = sum + i;
+			cnt = cnt + 1;
+		}
+	}
+
+	printf("%d", sum);
+	printf("\n%d", cnt);*/
+
+	// 입력받은 정수 10개 중 7에 가장 가까운 숫자를 찾는 알고리즘
+	/*int a[10];
+	int b[10];
+	int dif,j = 0;
+	int s_dif = INT_MAX;
+	
+	for (int i = 0; i < 10; i++)
+	{
+		scanf_s("%d", &a[i]);
+		dif = a[i] - 7;
+		if (dif < 0)
+		{
+			dif = dif * -1;
+		}
+		if (s_dif == dif)
+		{
+			j= j+1;
+			b[j] = a[i];
+			
+		}
+		else if (s_dif > dif)
+		{
+			s_dif = dif;
+			j = 0;
+			b[j] = a[i];
+		}
+	}
+	printf("결과 출력\n");
+	for (int x = 0; x <= j; x++)
+	{
+		printf("%d\n", b[x]);
+	}*/
+	//--------------------------------
+
+	// 오름차순 정렬
+	/*int a[10], tmp;
+	
+	for (int i = 0; i < 10; i++)
+	{
+		scanf_s("%d", &a[i]);
+	}
+
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			if (a[i]<a[j])
+			{
+				tmp = a[i];
+				a[i] = a[j];
+				a[j] = tmp;
+			}
+		}
+	}
+	printf("결과확인\n");
+	for (int x = 0; x < 10; x++)
+	{
+		printf("%d\n", a[x]);
+	}*/
+
+	//------------------------------
+
+	
+}
--- second/second/pointer.c
+++ second/second/pointer.c
@@ -1,44 +1,44 @@
-#include <stdio.h>
-main()
-{
-	/*double* a;
-	double b, tmp;
-	tmp = 3.14159;
-	a = &tmp;
-	b = *a;
-
-	printf("%f", b);*/
-	
-	/*int* a,b;
-	
-	a = &b;
-	b = 1;
-	printf("%p\n", a);
-	
-	*a++;
-	printf("%d %p %d", b, a, *a);*/ // b = 1, a = 주소값, *a= 쓰레기값
-	
-/*	int* a, b;
-
-	a = &b;
-	b = 1;
-	printf("%p\n", a);
-
-	(*a)++;
-	printf("%d %p %d", b, a, *a);*/  // b =2 , a = 주소값 a =2
-
-	int* a, b;
-
-	a = &b;
-	b = 1;
-	printf("%p\n", a);
-
-	++*a;
-	printf("%d %p %d", b, a, *a); // b= 2 , a = 주소값 , a = 2
-	
-
-
-
-
-
-}
+//#include <stdio.h>
+//main()
+//{
+//	/*double* a;
+//	double b, tmp;
+//	tmp = 3.14159;
+//	a = &tmp;
+//	b = *a;
+//
+//	printf("%f", b);*/
+//	
+//	/*int* a,b;
+//	
+//	a = &b;
+//	b = 1;
+//	printf("%p\n", a);
+//	
+//	*a++;
+//	printf("%d %p %d", b, a, *a);*/ // b = 1, a = 주소값, *a= 쓰레기값
+//	
+///*	int* a, b;
+//
+//	a = &b;
+//	b = 1;
+//	printf("%p\n", a);
+//
+//	(*a)++;
+//	printf("%d %p %d", b, a, *a);*/  // b =2 , a = 주소값 a =2
+//
+//	int* a, b;
+//
+//	a = &b;
+//	b = 1;
+//	printf("%p\n", a);
+//
+//	++*a;
+//	printf("%d %p %d", b, a, *a); // b= 2 , a = 주소값 , a = 2
+//	
+//
+//
+//
+//
+//
+//}
